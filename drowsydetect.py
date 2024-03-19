c81fe4e76d33a43f63a9faf96382741fbab60bf1
KVSREYAS <97829312+KVSREYAS@users.noreply.github.com>
2024-03-19 10:01:55 +0530

    Add files via upload

---
+++ drowsydetect.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+import torch
+import yolov5
+import numpy as np
+model = yolov5.load('/Users/sreyaskv/PycharmProjects/Drowsinessdetector/venv/share/drowsyfinal.pt')
+print(type(model))
+cap=cv2.VideoCapture(0)
+while cap.isOpened():
+    ret,frame=cap.read()
+    result=model(frame)
+    cv2.imshow('YOLO', np.squeeze(result.render()))
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
